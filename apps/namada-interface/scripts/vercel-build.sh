856f9a73ba23b0c23c75f7bff2fa37b1b449d2a6
gnosed <hiwz2ster@gmail.com>
2024-01-29 19:03:05 +0100

    fix: infinite loop

--- apps/namada-interface/scripts/vercel-build.sh
+++ apps/namada-interface/scripts/vercel-build.sh
@@ -13,11 +13,11 @@ echo "Installing wasm-pack..."
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
 
 # Build the WASM package
-echo "Building wasm-parser..."
+# echo "Building wasm-parser..."
 # Build wasm-parser 
-yarn run build:wasm
+# yarn run build:wasm
 
 # Complete the production build
-echo "Build static frontend client..."
+# echo "Build static frontend client..."
 # Build static html for the react client
-yarn run build
+# yarn run build
